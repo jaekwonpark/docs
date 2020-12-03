1e62fdfabd5645b8781e242a18d1a4bbdffbd4da
Jae Park <jae.park@nutanix.com>
2020-12-03 13:38:07 -0800

    version 20

--- cmdlet-help/Update-Help.sh
+++ cmdlet-help/Update-Help.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pwsh -Command 'New-ExternalHelp -Path "./md/en-US" -OutputPath "xml/en-US" -Force'
-lcab ./xml/en-US/* Nutanix.Prism.Common_90f6d4c4-e5b2-4ce4-9d79-6b6afefe9aa6_en-US_HelpContent.cab
+lcab -n ./xml/en-US/* Nutanix.Prism.Common_90f6d4c4-e5b2-4ce4-9d79-6b6afefe9aa6_en-US_HelpContent.cab
 zip -j Nutanix.Prism.Common_90f6d4c4-e5b2-4ce4-9d79-6b6afefe9aa6_en-US_HelpContent.zip ./xml/en-US/*
